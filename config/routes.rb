b674af0b204355fe2d7b8daa52cad1170e174217
exit <zrmartinezg@gmail.com>
2021-03-15 16:32:04 -0500

    add sessions controller, sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   
+  
   root "static_pages#home"
   get 'static_pages/home'
-  get '/signup',  to: 'users#new'
+  get '/signup',     to: 'users#new'
+  get "/login",      to:  "sessions#new"
+  post "/login",     to:  "sessions#create"  
+  delete "/logout",  to:  "sessions#destroy"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
