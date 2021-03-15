4c44383f91d5a2db33dc759fd468b54e6cb8d873
exit <zrmartinezg@gmail.com>
2021-03-14 22:51:08 -0500

    Add user controller with new method, update routes to signup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
+RSpec.describe "Users", type: :request do
+  describe "GET /new" do
     it "returns http success" do
-      get "/static_pages/home"
+      get "/users/new"
       expect(response).to have_http_status(:success)
     end
   end
-
 
 end
