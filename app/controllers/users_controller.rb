249bf5a4c70f50d09c451d4b080e666b4f1ef8e7
exit <zrmartinezg@gmail.com>
2021-03-15 16:04:06 -0500

    Add flash message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Time trac"
+      redirect_to @user
     else 
       render "new"
     end
