14b9eaedae6999a0b942224fb95ce4bef54566cb
exit <zrmartinezg@gmail.com>
2021-03-15 17:18:17 -0500

    Add create method in session controller, login method in sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else 
+      flash.now[:danger] = 'Invalid email or password'
+      render "new"      
+    end
+  end
+
+  def destroy
+
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,6 @@
 module SessionsHelper
+
+    def log_in(user)
+        session[:user_id] = user.id
+    end
 end
