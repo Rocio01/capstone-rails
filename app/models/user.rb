18ce31e2288288669c53dd318a9266e93d3e08b5
exit <zrmartinezg@gmail.com>
2021-03-14 23:31:50 -0500

    Add validations user model, tests in user_spec.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: {maximum: 50}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+    validates :email, presence: true, length: {maximum: 99},
+                                                format: { with: VALID_EMAIL_REGEX },
+                                                uniqueness: true
+    
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) { User.create!(name: 'wilmer', email: 'wamg@example.com') }
+  let(:wrong_user) { User.create!(name: ' ', email: ' ') }
+
+     it 'user can be created' do     
+       expect(user).to be_valid
+     end
+
+     it "name should be present" do
+       user.name = " "
+       expect(user).to_not be_valid
+     end
+
+     it "email should be present" do
+       user.email = " "
+       expect(user).to_not be_valid
+     end
+
+     it "name should not be too long" do
+       user.name = "a" * 51
+       expect(user).to_not be_valid
+     end
+
+     it "email should not be too long" do
+       user.email = "a" * 100 + "@gmail.com"
+       expect(user).to_not be_valid
+     end
+
+     it "email validation should reject invalid addresses" do
+       invalid_addresses = %w[user@example,com user_at_foo.org 
+                              user.name@example. foo@bar_baz.com foo@bar+baz.com]
+      
+       invalid_addresses.each do |w|
+           user.email = invalid_addresses                                     
+           expect(user).to_not be_valid
+       end
+     end
+
+     it "email addresses should be unique" do
+       duplicate_user = user.dup      
+       user.save
+       expect(duplicate_user).to_not be_valid
+     end
+
+    # it "password should be present" do
+    #   user.password = " "
+    #   expect(user).to_not be_valid
+    # end
+
+    # it "password should be have a minimum length" do
+    #   user.password = "a" * 5
+    #   expect(user).to_not be_valid
+    # end
+  
+
+ 
 end
