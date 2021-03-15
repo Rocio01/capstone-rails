7658d3a2096d33483319fbd62db73ee092a8464f
exit <zrmartinezg@gmail.com>
2021-03-15 11:57:37 -0500

    Add resources tu users, show methos, gravatar

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get 'static_pages/home'
   get '/signup',  to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
