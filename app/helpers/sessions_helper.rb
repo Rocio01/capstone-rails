7b2fe5448ed7a45700a5df15996039ccae0d3c4a
exit <zrmartinezg@gmail.com>
2021-03-15 20:14:25 -0500

    Add current_user method in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,11 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id] )
+        end
+    end
+
 end
