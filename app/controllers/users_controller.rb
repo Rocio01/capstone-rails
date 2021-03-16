e08e052e7bfa931eed819b36fa62149a6d719d1d
exit <zrmartinezg@gmail.com>
2021-03-16 15:44:40 -0500

    Add edit, update methods in user controller, add edit form, add allow_nil in user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,22 @@ class UsersController < ApplicationController
     end
 
   end
+
+def edit
+  @user = User.find(params[:id])
+end
+
+def update
+  @user = User.find(params[:id])
+  if @user.update(user_params)
+     flash[:success] = "Profile updated"
+      redirect_to @user
+  else
+     render "edit"
+  end
+end
+
+
 private
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
                                                 format: { with: VALID_EMAIL_REGEX },
                                                 uniqueness: true
     has_secure_password
-    validates :password, presence: true, length: {minimum: 6}
+    validates :password, presence: true, length: {minimum: 6},
+                                                 allow_nil: true   
 
 end
