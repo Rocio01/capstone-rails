88df89bec376b46e19865b2b84dcdc403a19c1f0
exit <zrmartinezg@gmail.com>
2021-03-17 16:01:05 -0500

    Add Group model, index on the user_id and created_at columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_044139) do
+ActiveRecord::Schema.define(version: 2021_03_17_205134) do
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_groups_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2021_03_15_044139) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "groups", "users"
 end
