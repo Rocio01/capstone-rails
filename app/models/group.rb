651430d6778823af08291a57c5b82c0726fae329
exit <zrmartinezg@gmail.com>
2021-03-17 16:46:26 -0500

    Add tests and validations to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: {maximum: 50}
+  validates :icon, presence: true
+  validates :user_id, presence: true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+    let(:user) { User.create!(name: 'example', email: 'wamg@example.com',
+                                       password: "foobar", password_confirmation: "foobar") }
+    let(:group) {  Group.create!(name: "Example Group", icon: "example icon", user_id: user.id ) }
+
+        it 'Group should be valid' do     
+             expect(group).to be_valid
+        end
+
+        it "name should be present" do
+             group.name = nil
+             expect(group).to_not be_valid
+         end
+
+        it "logo should be present" do
+             group.icon = nil
+             expect(group).to_not be_valid
+        end
+
+        it "user id should be present" do
+            group.user_id = nil
+             expect(group).to_not be_valid
+         end
+
+        it "name should not be too long" do
+             user.email = "a" * 51
+            expect(user).to_not be_valid
+        end
+
+      
+ end
