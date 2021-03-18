e34c37fa2886814f8ce105e782a4c5d9348b8580
exit <zrmartinezg@gmail.com>
2021-03-17 23:59:32 -0500

    Add form in new.html.erm in groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,27 @@
 class GroupsController < ApplicationController
+    
+    before_action :logged_in_user, only: [:index,  :create, :destroy]
+ 
+    def new
+       @group = Group.new
+    end
+    
     def index
         @groups = Group.all
     end
+    
+
+    def create
+
+    end
+
+
+    def destroy
+
+    end
+
+  private
+  
+
+
 end
