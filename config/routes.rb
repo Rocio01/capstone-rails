cd9044a5ff0d1208533d50cf34165d1a225ef0fa
exit <zrmartinezg@gmail.com>
2021-03-17 21:26:28 -0500

    Add groups controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post "/login",     to:  "sessions#create"  
   delete "/logout",  to:  "sessions#destroy"
   resources :users
+  resources :groups
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
