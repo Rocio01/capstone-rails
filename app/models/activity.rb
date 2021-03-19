20c8888fe850050e2ca3878215aaa913173028f6
exit <zrmartinezg@gmail.com>
2021-03-18 23:56:07 -0500

    Add Activity model

---
+++ app/models/activity.rb
@@ -0,0 +1,4 @@
+class Activity < ApplicationRecord
+  belongs_to :user
+  belongs_to :group
+end
---
+++ db/migrate/20210319044013_create_activities.rb
@@ -0,0 +1,14 @@
+class CreateActivities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :activities do |t|
+      t.string :name
+      t.integer :tiempo
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :activities, [:user_id, :created_at]
+    add_index :activities, [:group_id, :created_at]
+  end
+end
