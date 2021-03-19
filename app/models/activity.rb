ea31bdda7a8038d19fb9476d510ac72e8bbdfd8f
exit <zrmartinezg@gmail.com>
2021-03-19 12:22:25 -0500

    Delete model

--- app/models/activity.rb
+++
@@ -1,8 +0,0 @@
-class Activity < ApplicationRecord
-  belongs_to :user
-  belongs_to :group, optional: true
-  validates :name, presence: true, length: {maximum: 50}
-  validates :tiempo, presence: true
-  validates :user_id, presence: true
-  
-end
