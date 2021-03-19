d567d07bf3b1450f9b38adeaa50059eb3f989133
exit <zrmartinezg@gmail.com>
2021-03-19 15:32:57 -0500

    Add tests and validations for activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
   belongs_to :user
   belongs_to :group, optional: true
+  validates :name, presence: true, length: {maximum: 50}
+  validates :activity_time, presence: true
+  validates :user, presence: true
 end
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Activity, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    let(:user) { User.create!(name: 'wilmer', email: 'wamg@example.com',
+        password: "foobar", password_confirmation: "foobar") }
+    let(:group) {  Group.create(name: "Example Group", icon: "example icon", user_id: user.id ) }
+    let(:activity) {  Activity.create(name: "Example activity", activity_time: 1 , user_id: user.id, group_id: group.id ) }
+
+    it 'activity can be created' do     
+      expect(activity).to be_valid
+    end
+
+    it "name should be present" do
+      activity.name = " "
+      expect(activity).to_not be_valid
+    end
+
+    it "activity_time should be present" do
+     activity.activity_time = " "
+     expect(activity).to_not be_valid
+    end
+
+    it "name should not be too long" do
+      activity.name = "a" * 51
+      expect(activity).to_not be_valid
+    end
+
+    it "user_id should be present" do
+      activity.user_id = nil
+      expect(activity).to_not be_valid
+    end
+
+    it "group_id should be optional" do
+      activity.group_id = nil
+      expect(activity).to  be_valid
+    end
+
+
 end
