92fb82eeca1c7bce8622b852dea09c2f694cf8fe
exit <zrmartinezg@gmail.com>
2021-03-20 18:56:10 -0500

    Add before action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :external_activities, :group_activities ]
 
   def show
     @user = User.find(params[:id])
