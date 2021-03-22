1a67d9d2e8f895ce14e0aea4d240c164b94a660c
exit <zrmartinezg@gmail.com>
2021-03-22 14:53:06 -0500

    Fix style errors

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,5 @@
 class ActivitiesController < ApplicationController
-  before_action :logged_in_user, only: %i[create destroy]
+  before_action :logged_in_user, only: %i[new create destroy]
   before_action :correct_activity, only: :destroy
 
   def new
