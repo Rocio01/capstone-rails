9234691404e71fe9c065459184dc58c9eaf08ebc
exit <zrmartinezg@gmail.com>
2021-03-24 11:46:40 -0500

    Add Eager Loading for prevent n + 1 problem

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   has_many :activities, dependent: :destroy
   belongs_to :user
-  default_scope -> { order(name: :asc).limit(20) }
+  default_scope -> { order(name: :asc).includes(:user).limit(20) }
   validates :name, presence: true, length: { maximum: 50 }
   validates :icon, presence: true
   validates :user_id, presence: true
