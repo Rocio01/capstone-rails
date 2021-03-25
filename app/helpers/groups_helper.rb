bcaa5571674c9cb82bd73a5fbab5612af6d221df
exit <zrmartinezg@gmail.com>
2021-03-25 14:58:26 -0500

    Fix rubocop ofenses

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,10 +1,8 @@
 module GroupsHelper
+  def delete_link(group)
+    return unless group.user_id == current_user.id
 
- def delete_link(group)
-    if group.user_id == current_user.id 
-            link_to "delete", group, method: :delete, 
-                                        data: { confirm: "Are you sure?"} 
-    end 
- end
-
+    link_to 'delete', group, method: :delete,
+                             data: { confirm: 'Are you sure?' }
+  end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,15 +1,9 @@
 module StaticPagesHelper
-    def signup_btn
-        unless logged_in?
-            link_to "Sign up", signup_path, class: "btn btn-lg btn-success"
-            
-        end
-    end
-    def login_link
-        unless logged_in?            
-            tag.h5(link_to "Log in", login_path)
-        end
-    end    
- 
-end
+  def signup_btn
+    link_to 'Sign up', signup_path, class: 'btn btn-lg btn-success' unless logged_in?
+  end
 
+  def login_link
+    tag.h5(link_to('Log in', login_path)) unless logged_in?
+  end
+end
