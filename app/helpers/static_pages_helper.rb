f0e67cd41dc3af3d7f61f8c008229269a65b7911
exit <zrmartinezg@gmail.com>
2021-03-25 14:53:07 -0500

    Add login_link, signup_btn methods in Static Pages helper

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,15 @@
 module StaticPagesHelper
+    def signup_btn
+        unless logged_in?
+            link_to "Sign up", signup_path, class: "btn btn-lg btn-success"
+            
+        end
+    end
+    def login_link
+        unless logged_in?            
+            tag.h5(link_to "Log in", login_path)
+        end
+    end    
+ 
 end
+
